2e9dc000d7b5834e85cfd57ba114b43e13d65e80
ag <me@ag.com>
2023-07-29 01:53:16 +0500

    before github action

---
+++ src/functions/hello.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from Lambda!",
+      stage: process.env.STAGE,
+    }),
+  };
+};
